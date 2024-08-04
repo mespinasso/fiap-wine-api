977b4dd54fb291ba4093f1de64562ff6f6f6de10
matheus.espinasso <matheus.espinasso@nava.com.br>
2024-08-04 12:01:06 -0300

    Add metadata and contact info to FastAPI instance
    This update enhances the FastAPI instance with a title, summary, version, terms of service, and contact information. Additionally, it capitalizes the tag name for better readability.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+
 from app.api import production_api
 
-app = FastAPI()
+app = FastAPI(
+    title="Wine API 🍷",
+    summary="Embrapa Wine Database API",
+    version="1.0.0",
+    terms_of_service="http://vitibrasil.cnpuv.embrapa.br/",
+    contact={
+        "name": "Matheus Espinasso",
+        "url": "https://www.linkedin.com/in/matheuscoelhoespinasso"
+    }
+)
 
-app.include_router(production_api.router, prefix="/v1/production", tags=["production"])
+app.include_router(production_api.router, prefix="/v1/production", tags=["Production"])
